f8637218abe0538ece9ce42bf677cf1e5b95ef42
ds04011 <tommyhw97@gmail.com>
2025-06-23 18:14:01 +0900

    [ds04011] -메모 프로젝트 생성 - thymeleaf , mysql, mybatis 설정

---
+++ src/main/java/com/ds04011/memo/hello/Helloworld.java
@@ -0,0 +1,25 @@
+package com.ds04011.memo.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Helloworld {
+	
+	@ResponseBody
+	@GetMapping("/hello")
+	public String helloWorld() {
+			
+		String hello = "hello world";
+		
+		return hello;
+		
+	}
+	
+	@GetMapping("/helloworld")
+	public String hello() {
+		return "hello/hello.html";
+	}
+
+}
